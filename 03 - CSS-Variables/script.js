93813a1d9f50ad9869163808ad1a2a38955c953a
Max Jung <jung.moonki94@gmail.com>
2019-11-27 22:11:41 -0800

    updated script.js file to handle updates with change and mousemove

--- 03 - CSS-Variables/script.js
+++ 03 - CSS-Variables/script.js
@@ -1,8 +1,9 @@
 const inputs = document.querySelectorAll(".controls input");
 
 function handleUpdate() {
-  console.log(this.dataset);
+  const suffix = this.dataset.sizing || ""; // If there is no sizing, have nothing
+  document.documentElement.style.setProperty(`--${this.name}`, this.value+suffix);
 }
 
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
